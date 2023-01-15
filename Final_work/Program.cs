af0f1980b6885dd9c7cbf057a18d2826e5954473
Lizaveta <drago719@mail.ru>
2023-01-15 19:40:20 +0300

    task completed

--- Final_work/Program.cs
+++ Final_work/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] massiv = new string[] { "Hello", "2", "world", ":-)" };
+string[] result = new string[massiv.Length];
+int count = 0;
+for (int i = 0; i < massiv.Length; i++)
+{
+    string value = massiv[i];
+    if (value.Length <= 3)
+    {
+        result[count] = value;
+        count++;
+    }
+}
+Console.WriteLine(string.Join(" ", result));
