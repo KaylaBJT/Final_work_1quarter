918fd548a50786ab647b053c341799caf97f5cb4
Lizaveta <drago719@mail.ru>
2023-01-18 21:06:52 +0300

    Readme added

--- Final_work/Program.cs
+++ Final_work/Program.cs
@@ -1,13 +1,13 @@
-﻿string[] massiv = new string[] { "Hello", "2", "world", ":-)" };
-string[] result = new string[massiv.Length];
+﻿string[] Rick = new string[] { "Hello", "2", "world", ":-)" };
+string[] Morty = new string[Rick.Length];
 int count = 0;
-for (int i = 0; i < massiv.Length; i++)
+for (int i = 0; i < Rick.Length; i++)
 {
-    string value = massiv[i];
+    string value = Rick[i];
     if (value.Length <= 3)
     {
-        result[count] = value;
+        Morty[count] = value;
         count++;
     }
 }
-Console.WriteLine(string.Join(" ", result));
+Console.WriteLine(string.Join(" ", Morty));
